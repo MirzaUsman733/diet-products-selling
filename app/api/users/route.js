7eaf0bd7fad31d7da04fb85f0eedb62edb6d83ea
MirzaUsman733 <125288010+MirzaUsman733@users.noreply.github.com>
2024-01-20 16:04:11 +0500

    Make the website mature

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -8,7 +8,6 @@ export async function POST(req) {
         const userData = await User.find({}).select("_id name email role");
         return NextResponse.json({ userData });
     } catch (error) {
-        console.error(error);
         return NextResponse.error({ status: 500, message: 'Internal Server Error' });
     }
 }
