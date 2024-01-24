e0921c61607e31e62164472245ae074e436bad50
MirzaUsman733 <125288010+MirzaUsman733@users.noreply.github.com>
2024-01-24 17:06:10 +0500

    Home Caourosel add Perfectly

--- app/components/homePageComponents/Carousel.js
+++ app/components/homePageComponents/Carousel.js
@@ -1,69 +1,96 @@
-// components/Carousel.js
-import React from 'react';
+'use client'
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const Carousel = () => {
+const certifications = [
+  {
+    id: 1,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 2,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 4,
+    imageUrl:
+      'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 5,
+    imageUrl:
+      'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 6,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 7,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 8,
+    imageUrl:
+      'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 9,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 10,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 11,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+  {
+    id: 12,
+    imageUrl: 'https://via.placeholder.com/600x300',
+  },
+];
+
+export default function MicrosoftSlider() {
+
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 300,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: <CustomPrevArrow />,
-    nextArrow: <CustomNextArrow />,
-  };
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // fade: true,
+    // cssEase: 'linear'
 
-  const CustomPrevArrow = (props) => {
-    const { className, onClick } = props;
-    return (
-      <div className={className} onClick={onClick}>
-        <span role="img" aria-label="Previous" style={{ fontSize: '24px' }}>
-          &#x2190;
-        </span>
-      </div>
-    );
   };
-
-  const CustomNextArrow = (props) => {
-    const { className, onClick } = props;
-    return (
-      <div className={className} onClick={onClick}>
-        <span role="img" aria-label="Next" style={{ fontSize: '24px' }}>
-          &#x2192;
-        </span>
-      </div>
-    );
-  };
-
   return (
-    <div className="m-auto">
-      <Slider {...settings}>
-        <div>
-          <img
-            src="https://via.placeholder.com/600x300"
-            alt="Slide 1"
-            style={{ width: '95vw', height: '70vh', margin: 'auto' }}
-          />
-        </div>
-        <div>
-          <img
-            src="https://via.placeholder.com/600x300"
-            alt="Slide 2"
-            style={{ width: '95vw', height: '70vh', margin: 'auto' }}
-          />
-        </div>
-        <div>
-          <img
-            src="https://via.placeholder.com/600x300"
-            alt="Slide 3"
-            style={{ width: '95vw', height: '70vh', margin: 'auto' }}
-          />
-        </div>
-      </Slider>
-    </div>
-  );
-};
+    <div className=''>
+      <div className="text-center">
+        <Slider {...settings}>
+          {certifications.map((certification, index) => (
+            <div className="mb-4" key={certification.id}>
+                  
+              <div >
+                <img
+                  className="homeCertificationSliderContent"
+                  src={certification.imageUrl}
+                  alt=""
+                />
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
 
-export default Carousel;
+    </div>
+  )
+}
