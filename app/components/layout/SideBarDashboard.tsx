c154b7cd89264f107150a3d27bb5d4244b3bac81
MirzaUsman733 <125288010+MirzaUsman733@users.noreply.github.com>
2024-01-31 14:24:27 +0500

    starting adding link on the sidebar

--- app/components/layout/SideBarDashboard.tsx
+++ app/components/layout/SideBarDashboard.tsx
@@ -19,7 +19,8 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-
+import Link from "next/link";
+import HomeIcon from "@mui/icons-material/Home";
 const drawerWidth = 240;
 
 const openedMixin = (theme: Theme): CSSObject => ({
@@ -156,28 +157,22 @@ export default function SideBarDashboard({ children }: SideBarDashboard) {
         </DrawerHeader>
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? "initial" : "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+            <ListItemButton component={Link} href="/dashboard/users">
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+          <ListItemButton component={Link} href="/dashboard/users">
+            <ListItemIcon>{/* <HomeIcon /> */}</ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItemButton>
+          <ListItemButton component={Link} href="/dashboard/menu-items">
+            <ListItemIcon>{/* <DashboardIcon /> */}</ListItemIcon>
+            <ListItemText primary="Add Products" />
+          </ListItemButton>
+          <ListItemButton component={Link} href="/dashboard/orders">
+            <ListItemIcon>{/* <SettingsIcon /> */}</ListItemIcon>
+            <ListItemText primary="Orders" />
+          </ListItemButton>
         </List>
         <Divider />
         <List>
